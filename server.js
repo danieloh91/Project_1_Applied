da93eb5453da81a50c1dd284c93743544207a276
danieloh91 <daniel.oh.91@gmail.com>
2016-07-21 12:22:30 -0700

    made server.js connections to bower_components to display on local host

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-app.listen(process.env.PORT || 3000);
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    db = require('./models');
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.use(express.static(__dirname + '/public'));
+
+app.use('/vendor', express.static(__dirname + '/bower_components'));
+
+app.get('/', function homepage (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express server is running on http://localhost:3000/');
+});
