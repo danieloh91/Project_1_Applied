144203013aee8fe36a4ac627c88b9e4d0db8dc01
danieloh91 <daniel.oh.91@gmail.com>
2016-07-22 10:32:20 -0700

    added positions to seed

--- seed.js
+++ seed.js
@@ -1,7 +1,18 @@
 var db = require("./models");
 
-var positions = [
+var positionsList = [
   {
-    
+    position_name: 'Software Engineer - Release',
+    location: 'San Francisco, CA',
+    referral: false,
+    job_url: 'https://www.dropbox.com/jobs/listing/91413',
+    _company: Company._id
+  },
+  {
+    position_name: 'Software Engineer, Full Stack (RoR)',
+    location: 'San Francisco, CA',
+    referral: true,
+    job_url: 'https://www.smartrecruiters.com/UserTesting/92909872-software-engineer-full-stack-ror-',
+    _company: Company._id
   }
 ];
