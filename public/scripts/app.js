4b4621667ec94a82cb54dd59c374a2f2f08485a0
danieloh91 <daniel.oh.91@gmail.com>
2016-07-23 12:36:50 -0700

    seeded position data to appear on html

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
-  $.get.on('click', function() {
-    
+  var jobHtml =$('#job-template').html();
+      jobsTemplate = Handlebars.compile(jobHtml);
+
+  //show jobs
+  $.get('/api/positions').success(function(positions) {
+    positions.forEach(function (position) {
+      renderPosition(position);
+    });
   });
 });
+
+var jobsTemplate;
+
+function renderPosition(position) {
+  var html = jobsTemplate(position);
+  $('#job-postings').prepend(html);
+}
