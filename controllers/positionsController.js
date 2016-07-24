408f7042cf09c33a63abb77b8f9e01a5b26f6546
danieloh91 <daniel.oh.91@gmail.com>
2016-07-24 01:11:00 -0700

    company and position information properly saves and partially displays on html

--- controllers/positionsController.js
+++ controllers/positionsController.js
@@ -9,50 +9,35 @@ function index(req, res) {
   });
 }
 
+function createPosition(res, err, company, position) {
+  if (err) res.json(err);
+  else {
+    position.company_id = company._id;
+    position.save();
+    company._positions.push(position._id);
+    company.save();
+    res.json(position);
+  }
+}
+
 function create(req, res) {
   db.Company.find({company_name: req.body.company}, function(err, companies){
     // scenario 1: error connecting to db which returns err.
     // scenioar 2: company_name is not yet in db which returns []
     // scenario 3: company_name is in db which returns [company_obj]
-
     if (err) res.json(err);
-    // else if (conditional) { code you have under err beneath }
     else if (companies.length===0){
       db.Company.create(req.body, function(err, company){
         if (err) res.json(err);
         db.Position.create(req.body, function(err, position){
-          // res.json(company);
-          // ^^^ giving an array with company object.
-          // THIS IS NOT WHAT WE WANT.
-          // if you validate that you can never have repeat company, then you can fix this by doing this vvv
-
-          if (err) res.json(err);
-          else {
-            position.company_id = company._id;
-            position.save();
-            company._positions.push(position._id);
-            company.save();
-            res.json(position);
-          }
+          createPosition(res, err, company, position);
         });
       });
     }
     else {
       var company = companies[0];
       db.Position.create(req.body, function(err, position){
-        // res.json(company);
-        // ^^^ giving an array with company object.
-        // THIS IS NOT WHAT WE WANT.
-        // if you validate that you can never have repeat company, then you can fix this by doing this vvv
-
-        if (err) res.json(err);
-        else {
-          position.company_id = company._id;
-          position.save();
-          company._positions.push(position._id);
-          company.save();
-          res.json(position);
-        }
+        createPosition(res, err, company, position);
       });
     }
   });
