d1e031c4b6a4e97b4c89e0d6e444b4235f7bd9b8
justincastilla <pxlperfection@gmail.com>
2016-08-02 19:42:46 -0700

    continuing mission...

--- models/company.js
+++ models/company.js
@@ -1,9 +1,11 @@
 var mongoose = require("mongoose"),
     Schema = mongoose.Schema,
+    /* This is technically okay, but it makes other developers cry. Keep it how we did it in the other labs, you savage. -jc */
     CompanySchema = new Schema({
       company_name: String,
       hq: String,
       founded: Number,
+      /* TODO: Consider either creating a join table to connect the two models together or remove one of the foreign key relationships. -jc */
       _positions: [{type: mongoose.Schema.Types.ObjectId, ref: 'Position'}]
     });
 
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
 
       $jobsList = $('#job-postings');
 
+  /* TODO: Please remove commented code form production versions of your project -jc */
   // //show jobs
   // $.get('/api/positions').success(function(positions) {
   //   positions.forEach(function(position) {
@@ -16,12 +17,17 @@ $(document).ready(function() {
   //   });
   // });
 
+
+  /* TODO: Consider moving the callback function to an external, named function for a smaller and easier to read document ready section -jc */
+  /* TODO: Consider giving the function a name that informs the developer what the cb does.
   $('.add-job').click(function(e){
     e.preventDefault();
     $('.add-job').hide();
     $('.question').show();
   });
 
+  /* TODO: Consider moving the callback function to an external, named function for a smaller and easier to read document ready section -jc */
+  /* TODO: Consider giving the function a name that informs the developer what the cb does.*/
   $('.add-yes').click(function(e){
     e.preventDefault();
     $('.question').hide();
@@ -29,10 +35,13 @@ $(document).ready(function() {
   });
 
   //show companies
+  /* TODO: Consider moving the callback function to an external, named function for a smaller and easier to read document ready section -jc */
+  /* TODO: Consider giving the function a name that informs the developer what the cb does.*/
   $.get('/api/companies').success(function(companies) {
     // empty out the #job-postings ONLY when rerendering EVERYTHING (existing company, new position)
     // prepend when adding a single new company (new company, new position)
     companies.forEach(function(company) {
+      /* TODO: Please keep debugging code out of production versions of your project -jc */
       console.log('calling GET Request', company);
       renderCompany(company);
       renderDropdown(companies);
@@ -40,12 +49,17 @@ $(document).ready(function() {
   });
 
   //add job post
+  /* TODO: Consider moving the callback function to an external, named function for a smaller and easier to read document ready section -jc */
+  /* TODO: Consider giving the function a name that informs the developer what the cb does.*/
   $('#new-job-form').on("submit", function(e) {
+    /* TODO: Please keep debugging code out of production versions of your project -jc */
     console.log('calling on submit');
     e.preventDefault();
     var formData = $(this).serialize();
+    /* TODO: Please keep debugging code out of production versions of your project -jc */
     console.log('retreiving formData', formData);
     $.post('/api/positions', formData, function(company) {
+      /* TODO: Please keep debugging code out of production versions of your project -jc */
       console.log('position after POST', company);
       renderCompany(company);
     });
@@ -53,13 +67,17 @@ $(document).ready(function() {
   });
 
   // add to existing job post
+  /* TODO: Consider moving the callback function to an external, named function for a smaller and easier to read document ready section -jc */
+  /* TODO: Consider giving the function a name that informs the developer what the cb does.*/
   $('#addon-job-form').on("submit", function(e) {
     var id=$('#company_name').val();
+    /* TODO: Please keep debugging code out of production versions of your project -jc */
     console.log(id);
     e.preventDefault();
     $.ajax({
       method: 'PUT',
       url: '/api/companies/' + id,
+      /* TODO: For easier to read, consider assigning $(this).serialize() to a variable first. -jc */
       data: $(this).serialize(),
       success: handleEditCompanySuccess
     });
@@ -67,9 +85,12 @@ $(document).ready(function() {
   });
 
   //edit job post
+  /* TODO: Consider moving the callback function to an external, named function for a smaller and easier to read document ready section -jc */
+  /* TODO: Consider giving the function a name that informs the developer what the cb does.*/
   $jobsList.on('click', '.update-post', function(e) {
     e.preventDefault();
     var id= $(this).closest('.post').data('position-id');
+    /* TODO: Please keep debugging code out of production versions of your project -jc */
     console.log('id', id);
 
     var toggle_switch = $(this);
@@ -82,11 +103,14 @@ $(document).ready(function() {
     });
 
     // $('.collapse').show();
+    /* TODO: Consider moving the callback function to an external, named function for a smaller and easier to read document ready section -jc */
+    /* TODO: Consider giving the function a name that informs the developer what the cb does.*/
     $('#update-post').on("submit", function(e) {
       e.preventDefault();
       $.ajax({
         method: 'PUT',
         url: '/api/positions/' + id,
+        /* TODO: For easier to read, consider assigning $(this).serialize() to a variable first. -jc */
         data: $(this).serialize(),
         success: handleEditPositionSuccess
       });
@@ -98,9 +122,12 @@ $(document).ready(function() {
   });
 
   //delete job post
+  /* TODO: Consider moving the callback function to an external, named function for a smaller and easier to read document ready section -jc */
+  /* TODO: Consider giving the function a name that informs the developer what the cb does.*/
   $jobsList.on('click', '.delete-post', function(e) {
     e.preventDefault();
     var id= $(this).closest('.post').data('position-id');
+    /* TODO: Please keep debugging code out of production versions of your project -jc */
     console.log('id', id);
     $.ajax({
       method: 'DELETE',
@@ -113,6 +140,7 @@ $(document).ready(function() {
 
 function handleEditCompanySuccess(data){
   var editedCompanyId = data.id;
+  /* TODO: Streamline your code more by creating an url string to pass to the ajax get request instead of an in-place string assembly. -jc */
   $.get('/api/companies/' + editedCompanyId, function(data){
     renderCompany(data);
   });
@@ -120,6 +148,7 @@ function handleEditCompanySuccess(data){
 
 function handleEditPositionSuccess(data) {
   var editedPositionId = data._id;
+  /* TODO: Streamline your code more by creating an url string to pass to the ajax get request instead of an in-place string assembly. -jc */
   $.get('/api/companies/' + editedPositionId, function(data) {
     $('div[data-position-id=' + editedPositionId + ']').remove();
     renderCompany(data);
@@ -128,15 +157,17 @@ function handleEditPositionSuccess(data) {
 
 function handleDeletePositionSuccess(data) {
   var deletedPositionId = data._id;
+  /* TODO: Please keep debugging code out of production versions of your project -jc */
   console.log(data._id);
   $('div[data-position-id=' + deletedPositionId + ']').remove();
 }
 
+/* TODO: Please remove commented code form production versions of your project -jc */
 // function render(position) {
 //   var html = companiesTemplate(position);
 //   $jobsList.prepend(html);
 // }
-
+/* TODO: You are calling this funtion n times - where n is the number of companies that exist in your database. This includes grabbing hte template html and compiling it repeatedly n times.  Try refactoring the template selector and the compilation call out of this function and into your doc.ready. This will greatly reduce the workload of your browser logic. -JC */
 function renderDropdown(companies) {
   var dropdownHtml = $('#companies').html();
   var dropdownTemplate = Handlebars.compile(dropdownHtml);
@@ -147,6 +178,7 @@ function renderDropdown(companies) {
 }
 
 function renderCompany(company) {
+  /* TODO: Please remove commented code form production versions of your project -jc */
   // $jobsList.empty();
   var html = companiesTemplate(company);
   console.log('html: ', html);
