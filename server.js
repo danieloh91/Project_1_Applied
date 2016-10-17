ca98d0c83ce711ee4613fee9ba483e0ced5a41f4
danieloh91 <daniel.oh.91@gmail.com>
2016-10-17 15:32:57 -0700

    includes auth middle ware in server and creates user model

--- server.js
+++ server.js
@@ -2,13 +2,35 @@ var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
     db = require('./models'),
-    controllers = require('./controllers');
+    controllers = require('./controllers'),
+    mongoose = require('mongoose'),
+
+    cookieParser = require('cookie-parser'),
+    session = require('express-session'),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy;
 
 app.use(bodyParser.urlencoded({ extended: true}));
 
 app.use(express.static(__dirname + '/public'));
 
 app.use('/vendor', express.static(__dirname + '/bower_components'));
+
+
+// middleware for auth
+app.use(cookieParser());
+app.use(session({
+  secret: 'supersecretkey', // change this!
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// passport config
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get('/', function homepage (req, res) {
   res.sendFile(__dirname + '/views/index.html');
